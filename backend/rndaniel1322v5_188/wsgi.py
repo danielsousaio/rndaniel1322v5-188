3ef74fed76b7ab57af7e61a293e37b04fa6744a2
Daniel <daniel.s@crowdbotics.com>
2020-02-21 15:48:26 +0000

    Update models

--- backend/rndaniel1322v5_188/wsgi.py
+++ backend/rndaniel1322v5_188/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rndaniel1322v5_188.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rndaniel1322v5_188.settings")
 
 application = get_wsgi_application()
